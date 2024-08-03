f0344171eaa4581e2e10d8bc76ee3f14ebf7df56
nagarKeshav <nagark643@gmail.com>
2024-08-03 18:46:08 +0000

    container config

---
+++ 11_string.js
@@ -0,0 +1,7 @@
+let name = "keshav";
+let repocount = 50;
+
+console.log(`my name is ${name} and my repo count is ${repocount}`);
+
+const string4 = new String("A String object");
+console.log(string4);
---
+++ video10.js
@@ -0,0 +1,14 @@
+console.log("hello word");
+let userone = {
+    email: "user@gmail.com",
+    name: "user"
+}
+
+let usertwo = {
+    email: "keshu@gmail.com",
+    name: "keshav"
+}
+
+console.log(userone);
+console.log(usertwo.email);
+
