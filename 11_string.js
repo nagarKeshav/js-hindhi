683ca6cad5f553055bdda57e360a728acc8c98ed
nagarKeshav <nagark643@gmail.com>
2024-08-08 10:44:16 +0000

    math and num

--- 11_string.js
+++ 11_string.js
@@ -1,7 +1,36 @@
-let name = "keshav";
-let repocount = 50;
+// let name = "keshav";
+// let repocount = 50;
 
-console.log(`my name is ${name} and my repo count is ${repocount}`);
+// console.log(`my name is ${name} and my repo count is ${repocount}`);
 
-const string4 = new String("A String object");
-console.log(string4);
+// const string4 = new String("A String object");
+// console.log(string4);
+
+// let score = 100
+// console.log(score);
+
+// let balance = new Number(100)
+// console.log(balance)
+
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(3))
+
+// let otherscore =  123.987
+// console.log(otherscore.toPrecision(4))
+// let hunderds = 1000000000000000
+// console.log(hunderds.toLocaleString())
+
+
+
+
+//------------------------------------------------------<maths>-----------------------------------------------------------------------
+
+
+console.log(Math);
+
+let min  = 10;
+let max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
