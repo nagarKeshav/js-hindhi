f1ac77986c069436d00a4d5968153d330a406345
nagarKeshav <nagark643@gmail.com>
2024-08-09 12:47:25 +0000

    array

---
+++ 14_array.js
@@ -0,0 +1,23 @@
+// const myarry = new Array(1,2,3,4)
+// console.log(typeof myarry)
+// console.log(myarry)
+
+let myArray = [1,2,3,4,5]
+//myArray.push(6);
+//myArray.pop()
+//myArray.unshift(9)
+//myArray.shift()
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(4));
+
+const myn1 = myArray.slice(1,3);
+console.log("A" ,myn1);
+
+const myn2 = myArray.splice(1,3)
+console.log("B", myn2);
+
+console.log(myArray)
+
+
+
+
